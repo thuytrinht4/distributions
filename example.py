17e371af91cd08ad8516944323cf3391c9c5218b
Thuy Trinh <thuytrinht4@gmail.com>
2021-10-10 21:28:58 +0700

    initial commit

---
+++ example.py
@@ -0,0 +1,4 @@
+from distributions import Gaussian
+gaussian_one = Gaussian(25, 2)
+print(f'Gaussian_one mean: {gaussian_one.mean}')
+print('new distribution info: ', (gaussian_one + gaussian_one).__repr__())
